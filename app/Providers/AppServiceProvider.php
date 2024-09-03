f1aaa47cc5f846e951499e29461bf3d86a8a8962
EMCodeHUB <elgolazoshop@gmail.com>
2024-09-03 15:02:11 +0300

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // Asegúrate de importar el facade Schema
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Establecer la longitud predeterminada de las columnas de cadena para evitar el error de longitud de clave
+        Schema::defaultStringLength(191);
     }
 }
